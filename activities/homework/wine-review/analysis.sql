c779c520a02e6d9c3bcc9d97c3ac41630ddf62c1
pablovargasc <pablovargas862@gmail.com>
2019-11-06 18:50:11 -0600

    analysis.sql

--- activities/homework/wine-review/analysis.sql
+++ activities/homework/wine-review/analysis.sql
@@ -7,22 +7,92 @@
  */
 
 -- EX.1) Get the top 10 countries with more population density
+SELECT
+    name,
+    population / area_km2 density
 
+FROM
+    country
+WHERE area_km2 > 0
+ORDER BY
+    density DESC
+LIMIT 10
+;
 
 -- EX.2) Get the count of male/female tasters.
-
+-- SELECT DISTINCT te regresa los valores unicos
+SELECT
+    gender,
+    count(gender)
+FROM
+    taster
+WHERE
+      LOWER(gender) IN ('male', 'female')
+    -- LOWER(gender) != 'undefined' / NOT LIKE / LIKE '%male'
+GROUP BY gender
 
 -- EX.3) Get the percentage of male/female tasters.
-
+-- :: cambia de tipo de dato, (entera, decimal)
+WITH taster_valid AS (
+    SELECT
+        *
+    FROM
+        taster
+    WHERE
+        LOWER(gender) IN ('male', 'female')
+    ), taster_gender_agg AS (
+    SELECT
+        gender,
+        COUNT(*)::NUMERIC(7, 2) gender_sum
+    FROM
+        taster_valid
+    GROUP BY
+        gender
+    ), taster_total AS (
+    SELECT
+        count(*):: NUMERIC(7, 2) total
+    FROM
+        taster_valid
+    )
+SELECT
+    gender,
+    TRUNC(100 * gender_sum / total,2) percentage
+FROM
+    taster_gender_agg,
+    taster_total
+;
 
 -- EX.4) How many countries share the same first digit on their country-code?
 -- Show only those digits with more than 20 countries.
+SELECT DISTINCT LEFT(code, 1) FROM country
+-- where aplica antes del group by
+-- having aplica despues del group by
 
+SELECT
+    LEFT(code, 1) first_digit,
+    count(*) country_count
+FROM
+    country
+GROUP BY
+    first_digit
+HAVING
+    count(*) > 20
+;
 
 -- EX.5) Get the % of countries are not labeled as a trillion usd gdp and
 -- do have a null happiness_score.
+;
 
-
+SELECT
+    100 * COUNT(*) / MAX(t.total) Percentage
+FROM
+     country,
+     (SELECT count(*) total FROM country) t
+WHERE
+    LOWER(split_part(gdp_usd,' ',2)) != 'trillion'
+    --LOWER(gdp_usd) NOT LIKE '%trillion'
+    AND happiness_score IS NULL
+;
 -- COUNTRY ANALYSIS
 
 -- A) Get the average happiness_score of the countries labeled with a GDP
